06505e0e782dad99b515bd8c4579da8354ef5547
clovis <clovis.maniguet@gmail.com>
2015-11-21 18:48:10 +0100

    TimeLine

--- js/app/home.js
+++ js/app/home.js
@@ -1,6 +1,27 @@
 // Home
-$('.time').on('mouseenter', function(){
+$('#timeline > .time').on('mouseenter', function(){
 	if($(this).attr('data-gen')){
-		console.log($(this).attr('data-gen'));
+		$('#generation h3').html($(this).attr('data-gen'));
 	}
 })
+
+var TimeLine = function(){
+	
+	// Var
+	this.sizeIT; 
+	
+	// Function
+	this.init();
+}
+
+TimeLine.prototype.init = function(){
+	this.sizeIT = ($(window).innerWidth() - 140) / 8;
+
+	$('.interTime').css('width', this.sizeIT+'px');
+	
+	for(var x = 0; x <= this.sizeIT; x+=20){
+		$('.interTime').append('<div class="time"><div class="smallTimeL"></div><div class="smallTimeR"></div></div>');
+	}
+}
+
+new TimeLine();
