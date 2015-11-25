e981404eac238b889de7eb960bf872d5a44ca465
Thomas Victoria <thomas.victoria@hetic.net>
2015-11-25 19:21:19 +0100

    graph

--- js/app/displaydata.js
+++ js/app/displaydata.js
@@ -24,55 +24,62 @@ pokearticle.prototype.init = function(){
 
 pokearticle.prototype.show = function(){
 
+  console.log(this.data.reponse);
+  
   this.article.find('h2').html(this.data.reponse.name);
 
-  this.td[0].innerText = 'Height : ';
-  this.td[1].innerText = this.data.reponse.height;
-
-  this.td[2].innerText = 'Attack : ';
-  this.td[3].innerText = this.data.reponse.attack;
-
-  this.td[4].innerText = 'Weight : ';
-  this.td[5].innerText = this.data.reponse.weight;
-
-  this.td[6].innerText = 'Defense : ';
-  this.td[7].innerText = this.data.reponse.defense;
-
-  this.td[8].innerText = 'Speed : ';
-  this.td[9].innerText = this.data.reponse.speed;
+  $('#taille').html('Height : '+this.data.reponse.height);
+  $('#taille').css('height', (this.data.reponse.height*100)/145+'%');
+  $('#poid').html('Weight : '+this.data.reponse.weight);
+  $('#poid').css('height', (this.data.reponse.weight*100)/22044+'%');
+  $('#vitesse').html('Speed : '+this.data.reponse.speed);
+  $('#vitesse').css('height', (this.data.reponse.speed*100)/180+'%');
+  $('#attack').html('Attack : '+this.data.reponse.attack);
+  $('#attack').css('height', (this.data.reponse.attack*100)/180+'%');
+  $('#spe_attack').html('Special Attack : '+this.data.reponse.sp_atk);
+  $('#spe_attack').css('height', (this.data.reponse.sp_atk*100)/180+'%');
+  $('#defense').html('Defense : '+this.data.reponse.defense);
+  $('#defense').css('height', (this.data.reponse.defense*100)/230+'%');
+  $('#hp').html('HP : '+this.data.reponse.hp);
+  $('#hp').css('height', (this.data.reponse.hp*100)/255+'%');
+  $('#spe_def').html('Special Defense : '+this.data.reponse.sp_def);
+  $('#spe_def').css('height', (this.data.reponse.sp_def*100)/230+'%');
 
 }
 
 pokearticle.prototype.moveability = function(){
 
-  var self    = this.data,
-      selfFun = this.display,
-      selfid  = self.reponse.national_id,
-      ability = $.getJSON('../../data/ability.json'),
-      moves   = $.getJSON('../../data/moves.json');
+  var self          = this.data,
+      localSelector = this,
+      selfFun       = this.display,
+      selfid        = self.reponse.national_id,
+      ability       = $.getJSON('../../data/ability.json'),
+      moves         = $.getJSON('../../data/moves.json'),
+      type          = $.getJSON('../../data/types.json');
 
   console.log(selfid);
 
   $(this.detail).children('h2').html(self.name);
 
-  $.when(ability, moves).done(function(ability, moves){
+  $.when(ability, moves, type).done(function(ability, moves, type){
 
-    selfFun(ability[0], moves[0], selfid);
+    selfFun(ability[0], moves[0], type[0], selfid, localSelector);
 
   });
 }
 
-pokearticle.prototype.display = function(ability, moves, pokemonId){
+pokearticle.prototype.display = function(ability, moves, type, pokemonId, localSelector){
 
-  $('#abilities, #moves').empty();
+  $('#abilities, #moves, #types').empty();
 
   $('#abilities').append('Abilities :');
-  $('#moves').append('Moves :')
+  $('#moves').append('Moves :');
+  $('#types').append('Type :');
 
 
   for(i=1; i < Object.keys(ability).length; i++){
 
-    for(x=1; x < Object.keys(ability[i].pokemons).length; x++){
+    for(x=0; x < Object.keys(ability[i].pokemons).length; x++){
 
       if(ability[i].pokemons[x] == pokemonId){
 
@@ -108,6 +115,31 @@ pokearticle.prototype.display = function(ability, moves, pokemonId){
               id        = step.slice(0,lenght -1);
 
           $('#moves').append('<a href="#"><div class="ability" data-id="'+id+'" >'+ moves[y].name +'</div></a>')
+        }
+
+      }
+
+    }
+
+  }
+
+  for(y=1; y < Object.keys(type).length; y++){
+
+    if(y != 165){
+
+      for(z=1; z < Object.keys(type[y].pokemons).length; z++){
+        
+        if(type[y].pokemons[z] == pokemonId){
+
+          var link      = type[y].resource_uri,
+              delimiter = '/',
+              start     = 4,
+              tokens    = link.split(delimiter).slice(start),
+              step      = tokens.join(delimiter),
+              lenght    = step.length,
+              id        = step.slice(0,lenght -1);
+
+          $('#types').append('<a href="#"><div class="type" data-id="'+id+'" >'+ type[y].name +'</div></a>')
         }
 
       }
